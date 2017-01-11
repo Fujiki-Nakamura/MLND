457a7b8157d7011bc0323412afa4077a36c322fc
fujiki <abcdef.nakamura@gmail.com>
2017-01-11 08:26:43 +0000

    XGB, Stacking - param #3 - num_boost_round = 577

--- capstone/parameters.py
+++ capstone/parameters.py
@@ -1,12 +1,12 @@
 xgb_params = {
     'min_child_weight': 100,
     'max_depth': 12,
-    'eta': 0.03,
-    'colsample_bytree': 0.7,
-    'subsample': 0.7,
-    'alpha': 0,
+    'eta': 0.01,
+    'colsample_bytree': 0.5,
+    'subsample': 0.8,
+    'alpha': 1,
     'lambda': 1,
-    'gamma': 0,
+    'gamma': 1,
     'silent': 1,
     'seed': 0,
     'nthreads': -1
