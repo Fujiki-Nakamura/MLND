9f469b682f291a19fe685c4e930fdfebfae74c34
fujiki <abcdef.nakamura@gmail.com>
2017-01-11 10:29:02 +0900

    XGBoost best params so far: param No.1

--- capstone/parameters.py
+++ capstone/parameters.py
@@ -4,11 +4,11 @@ xgb_params = {
     'eta': 0.01,
     'colsample_bytree': 0.5,
     'subsample': 0.8,
-    'alpha': 0,
+    'alpha': 1,
     'lambda': 100,
-    'gamma': 0,
+    'gamma': 1,
     'silent': 1,
     'verbose_eval': True,
     'seed': 0,
-    'nthreads': 8
+    'nthreads': -1
 }
