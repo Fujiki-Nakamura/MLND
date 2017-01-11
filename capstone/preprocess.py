03bfac321db55b56191fcc5d276f83bcc13d57fb
fujiki <abcdef.nakamura@gmail.com>
2017-01-11 16:15:57 +0900

    Fix preprocess.py indent

--- capstone/preprocess.py
+++ capstone/preprocess.py
@@ -10,23 +10,23 @@ if __name__ == '__main__':
 
     for column in list(train.select_dtypes(include=['object']).columns):
 
-    # remove categorical values existing only in train data or test data
-    if train[column].nunique() != test[column].nunique():
-        set_train = set(train[column].unique())
-        set_test = set(test[column].unique())
-        remove_train = set_train - set_test
-        remove_test = set_test - set_train
-        remove = remove_train.union(remove_test)
+        # remove categorical values existing only in train data or test data
+        if train[column].nunique() != test[column].nunique():
+            set_train = set(train[column].unique())
+            set_test = set(test[column].unique())
+            remove_train = set_train - set_test
+            remove_test = set_test - set_train
+            remove = remove_train.union(remove_test)
 
-        def filter_cat(x):
-            if x in remove:
-                return np.nan
-            return x
+            def filter_cat(x):
+                if x in remove:
+                    return np.nan
+                return x
 
-        train_test[column] = train_test[column].apply(lambda x: filter_cat(x), 1)
+            train_test[column] = train_test[column].apply(lambda x: filter_cat(x), 1)
 
-    # Factorize the categorical features in lexicographical order
-    train_test[column] = pd.factorize(train_test[column].values, sort=True)[0]
+        # Factorize the categorical features in lexicographical order
+        train_test[column] = pd.factorize(train_test[column].values, sort=True)[0]
 
     train = train_test[train_test['loss'].notnull()]
     test = train_test[train_test['loss'].isnull()]
