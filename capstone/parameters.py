7b528d641b6fd83cb4e17b97428db161daa6cfdb
fujiki <abcdef.nakamura@gmail.com>
2017-01-11 15:28:33 +0900

    param No.3

--- capstone/parameters.py
+++ capstone/parameters.py
@@ -1,14 +1,13 @@
 xgb_params = {
     'min_child_weight': 100,
     'max_depth': 12,
-    'eta': 0.01,
-    'colsample_bytree': 0.5,
-    'subsample': 0.8,
-    'alpha': 1,
-    'lambda': 100,
-    'gamma': 1,
+    'eta': 0.03,
+    'colsample_bytree': 0.7,
+    'subsample': 0.7,
+    'alpha': 0,
+    'lambda': 1,
+    'gamma': 0,
     'silent': 1,
-    'verbose_eval': True,
     'seed': 0,
     'nthreads': -1
 }
