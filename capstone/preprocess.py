74d2a5f9f9db8b3de3c149a947ec04c2d9311eb8
Fujiki Nakamura <fujikinakamura@CaccoBpdMBA.local>
2017-01-16 18:28:26 +0900

    refactor & add make data removed and one hot function

--- capstone/preprocess.py
+++ capstone/preprocess.py
@@ -2,7 +2,42 @@ import numpy as np
 import pandas as pd
 
 
-if __name__ == '__main__':
+def make_data_removed_one_hot():
+    train = pd.read_csv('./data/train.csv')
+    test = pd.read_csv('./data/test.csv')
+    test['loss'] = np.nan
+    train_test = pd.concat([train, test])
+
+    for column in list(train.select_dtypes(include=['object']).columns):
+
+        # remove categorical values existing only in train data or test data
+        if train[column].nunique() != test[column].nunique():
+            set_train = set(train[column].unique())
+            set_test = set(test[column].unique())
+            remove_train = set_train - set_test
+            remove_test = set_test - set_train
+            remove = remove_train.union(remove_test)
+
+            def filter_cat(x):
+                if x in remove:
+                    return np.nan
+                return x
+
+            train_test[column] = train_test[column].apply(lambda x: filter_cat(x), 1)
+
+    # one-hot encode
+    train_test_removed_one_hot = pd.get_dummies(train_test)
+
+    train_removed_one_hot = \
+        train_test_removed_one_hot[train_test_removed_one_hot['loss'].notnull()]
+    test_removed_one_hot = \
+        train_test_removed_one_hot[train_test_removed_one_hot['loss'].isnull()]
+
+    train_removed_one_hot.to_csv('./data/train_removed_one_hot.csv', index=False)
+    test_removed_one_hot.to_csv('./data/test_removed_one_hot.csv', index=False)
+
+
+def make_data_removed_and_factorized():
     train = pd.read_csv('./data/train.csv')
     test = pd.read_csv('./data/test.csv')
     test['loss'] = np.nan
@@ -38,3 +73,8 @@ if __name__ == '__main__':
     # Create preprocessed data
     train.to_csv('data/train_preprocessed.csv', index=False)
     test.to_csv('data/test_preprocessed.csv', index=False)
+
+
+if __name__ == '__main__':
+    make_data_removed_and_factorized()
+    make_data_removed_one_hot()
