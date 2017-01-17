e2cb52282c6437f75647e9edd808644f18194d86
Fujiki Nakamura <fujikinakamura@CaccoBpdMBA.local>
2017-01-17 10:13:19 +0900

    make data one-hot (but not removed)

--- capstone/preprocess.py
+++ capstone/preprocess.py
@@ -2,39 +2,17 @@ import numpy as np
 import pandas as pd
 
 
-def make_data_removed_one_hot():
+def make_data_one_hot():
     train = pd.read_csv('./data/train.csv')
     test = pd.read_csv('./data/test.csv')
     test['loss'] = np.nan
-    train_test = pd.concat([train, test])
-
-    for column in list(train.select_dtypes(include=['object']).columns):
-
-        # remove categorical values existing only in train data or test data
-        if train[column].nunique() != test[column].nunique():
-            set_train = set(train[column].unique())
-            set_test = set(test[column].unique())
-            remove_train = set_train - set_test
-            remove_test = set_test - set_train
-            remove = remove_train.union(remove_test)
-
-            def filter_cat(x):
-                if x in remove:
-                    return np.nan
-                return x
-
-            train_test[column] = train_test[column].apply(lambda x: filter_cat(x), 1)
 
     # one-hot encode
-    train_test_removed_one_hot = pd.get_dummies(train_test)
+    train_one_hot = pd.get_dummies(train)
+    test_one_hot = pd.get_dummies(test)
 
-    train_removed_one_hot = \
-        train_test_removed_one_hot[train_test_removed_one_hot['loss'].notnull()]
-    test_removed_one_hot = \
-        train_test_removed_one_hot[train_test_removed_one_hot['loss'].isnull()]
-
-    train_removed_one_hot.to_csv('./data/train_removed_one_hot.csv', index=False)
-    test_removed_one_hot.to_csv('./data/test_removed_one_hot.csv', index=False)
+    train_removed_one_hot.to_csv('./data/train_one_hot.csv', index=False)
+    test_removed_one_hot.to_csv('./data/test_one_hot.csv', index=False)
 
 
 def make_data_removed_and_factorized():
@@ -77,4 +55,4 @@ def make_data_removed_and_factorized():
 
 if __name__ == '__main__':
     make_data_removed_and_factorized()
-    make_data_removed_one_hot()
+    make_data_one_hot()
