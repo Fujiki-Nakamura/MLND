1749d583b9b8ef7cbc7853caa6258db3e5a66084
fujiki <abcdef.nakamura@gmail.com>
2017-01-17 23:13:21 +0900

    xgb v2, 5-fold stacking, - param#1 - num_boost_round = 3000
    Start Fold 1
    End Fold 1 in 940.47509932518 s
    Start Fold 2
    End Fold 2 in 941.3017377853394 s
    Start Fold 3
    End Fold 3 in 942.4624512195587 s
    Start Fold 4
    End Fold 4 in 944.1762990951538 s
    Start Fold 5
    End Fold 5 in 937.7815134525299 s

---
+++ capstone/xgb_v2/stacking.py
@@ -0,0 +1,91 @@
+import argparse
+import datetime
+import time
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import KFold
+
+import xgboost as xgb
+
+from parameters import xgb_params
+from utils import create_dtrain_dtest
+from utils import create_features_and_labels
+from utils import evalerror
+from utils import fair_objective
+from utils import load_data
+
+
+# Argument 'model_name' is used as stacked predictions of out-of-fold predictions
+parser = argparse.ArgumentParser()
+parser.add_argument('model_name', metavar='model_name', type=str)
+args = parser.parse_args()
+model_name = args.model_name
+
+train_data = '../data/train_preprocessed.csv'
+test_data = '../data/test_preprocessed.csv'
+
+# XGBoost parameters
+num_boost_round = 3000 # best num round for param #1 is around 3000
+#early_stopping_rounds = 10 early_stopping_rounds parameter causes an error in train
+#verbose_eval = 100
+# KFold parameter
+n_splits = 5
+
+shift = 200
+
+out_of_fold_preds_list = []
+df_stacked_out_of_fold_preds = pd.DataFrame()
+stacked_preds_csv = './result/stacked_preds_{}.csv'.format(model_name)
+
+df_preds_tmp = pd.DataFrame()
+temporal_preds_csv = './result/temporal_preds_xgb.csv'
+
+
+if __name__ == '__main__':
+    train, test = load_data(train_data, test_data)
+    X_train, y_train, X_test = create_features_and_labels(train, test)
+
+    dtest = xgb.DMatrix(X_test)
+
+    kf = KFold(n_splits=n_splits, shuffle=False, random_state=None)
+
+    for i, (train_index, test_index) in enumerate(kf.split(X_train), start=1):
+        print('Start Fold {}'.format(i))
+        t0_fold = time.time()
+
+        X_train_prime, X_test_prime = X_train[train_index], X_train[test_index]
+        y_train_prime, y_test_prime = y_train[train_index], y_train[test_index]
+
+        dtrain_prime = xgb.DMatrix(X_train_prime, label=y_train_prime)
+        dtest_prime = xgb.DMatrix(X_test_prime)
+
+        gbdt = \
+        xgb.train(xgb_params,
+                  dtrain_prime,
+                  num_boost_round=num_boost_round,
+                  #early_stopping_rounds=early_stopping_rounds,
+                  #verbose_eval=verbose_eval,
+                  feval=evalerror,
+                  obj=fair_objective,
+                  )
+
+        # Out of fold prediction
+        out_of_fold_preds = np.exp(gbdt.predict(dtest_prime)) - shift
+        # Hold this prediction and save them later
+        out_of_fold_preds_list.append(out_of_fold_preds)
+
+        # temporal prediction
+        preds_tmp = np.exp(gbdt.predict(dtest)) - shift
+        df_preds_tmp['fold_{}'.format(i)] = preds_tmp
+
+        print('End Fold {} in {} s'.format(i, time.time() - t0_fold))
+
+    # Save the stacked out-of-fold predictions
+    df_stacked_out_of_fold_preds[model_name] = np.concatenate(out_of_fold_preds_list)
+    df_stacked_out_of_fold_preds\
+    .to_csv(stacked_preds_csv, index=False)
+    # Save the temporal predictions
+    df_preds_tmp.to_csv(temporal_preds_csv)
+Contact GitHub API Training Shop Blog About
