68e2d25e3e0fc6537142b29d43d2cd3e0735488a
fujiki <abcdef.nakamura@gmail.com>
2017-01-17 22:09:00 +0900

    3-layer v1 - with removed and one-hot data

---
+++ capstone/3_layer_v1/model.py
@@ -0,0 +1,21 @@
+# coding: UTF-8
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation
+from keras.layers.normalization import BatchNormalization
+
+
+def create_model(input_dim):
+    model = Sequential()
+
+    model.add(Dense(128, input_dim=input_dim))
+    model.add(Activation('relu'))
+
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+
+    # output layer
+    model.add(Dense(1))
+
+    model.compile(loss='mae', optimizer='adam')
+
+    return model
