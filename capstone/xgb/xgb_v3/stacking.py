46ac11baabb3f864180c2c15ed0d7a6111e0e0a6
Fujiki Nakamura <fujikinakamura@CaccoBpdMBA.local>
2017-01-18 11:58:58 +0900

    [Refactor] XGB stacking script

--- capstone/xgb/xgb_v3/stacking.py
+++ capstone/xgb/xgb_v3/stacking.py
@@ -1,5 +1,7 @@
 import argparse
 import datetime
+import os
+import sys
 import time
 
 import pandas as pd
@@ -9,6 +11,7 @@ from sklearn.model_selection import KFold
 
 import xgboost as xgb
 
+sys.path.append('../')
 from parameters import xgb_params
 from utils import create_dtrain_dtest
 from utils import create_features_and_labels
@@ -23,30 +26,35 @@ parser.add_argument('model_name', metavar='model_name', type=str)
 args = parser.parse_args()
 model_name = args.model_name
 
-train_data = './data/train_preprocessed.csv'
-test_data = './data/test_preprocessed.csv'
+# Data preparation
+data_dir = '../../data/'
+train_data = data_dir + 'train_preprocessed.csv'
+test_data = data_dir + 'test_preprocessed.csv'
+train, test = load_data(train_data, test_data)
+X_train, y_train, X_test = create_features_and_labels(train, test)
 
 # XGBoost parameters
-num_boost_round = 10000
+num_boost_round = 10
 early_stopping_rounds = 10
 verbose_eval = 100
+
 # KFold parameter
 n_splits = 5
 
 shift = 200
 
-out_of_fold_preds_list = []
+# placeholder
 df_stacked_out_of_fold_preds = pd.DataFrame()
-stacked_preds_csv = './result/stacked_preds_{}.csv'.format(model_name)
+df_stacked_out_of_fold_preds['id'] = train['id']
+out_of_fold_preds = np.zeros(X_train.shape[0])
 
 df_preds_tmp = pd.DataFrame()
-temporal_preds_csv = './result/temporal_preds_{}.csv'.format(model_name)
+df_preds_tmp['id'] = test['id']
+
+df_cross_validation = pd.DataFrame()
 
 
 if __name__ == '__main__':
-    train, test = load_data(train_data, test_data)
-    X_train, y_train, X_test = create_features_and_labels(train, test)
-
     dtest = xgb.DMatrix(X_test)
 
     kf = KFold(n_splits=n_splits, shuffle=False, random_state=None)
@@ -77,12 +85,12 @@ if __name__ == '__main__':
         print('best_iteration', gbdt.best_iteration)
         print('best_ntree_limit', gbdt.best_ntree_limit)
 
+        preds = np.exp(gbdt.predict(dtest_prime, ntree_limit=gbdt.best_ntree_limit)) - shift
         # Out of fold prediction
-        out_of_fold_preds = \
-        gbdt.predict(dtest_prime, ntree_limit=gbdt.best_ntree_limit)
-        out_of_fold_preds = np.exp(out_of_fold_preds) - shift
-        # Hold this prediction and save them later
-        out_of_fold_preds_list.append(out_of_fold_preds)
+        out_of_fold_preds[test_index] = preds
+        # CV result
+        trues = np.exp(y_test_prime) - shift
+        df_cross_validation.loc[i, 'val_mae'] = mean_absolute_error(trues, preds)
 
         # temporal prediction
         preds_tmp = gbdt.predict(dtest, ntree_limit=gbdt.best_ntree_limit)
@@ -90,10 +98,16 @@ if __name__ == '__main__':
         df_preds_tmp['fold_{}'.format(i)] = preds_tmp
 
         print('End Fold {} in {} s'.format(i, time.time() - t0_fold))
+        break
 
+    result_directory = './result/'
+    if not os.path.exists(result_directory):
+        os.makedirs(result_directory)
+    # Save the CV result
+    print('mean MAE on CV = ', df_cross_validation['val_mae'].mean())
+    df_cross_validation.to_csv(result_directory + 'cross_validation', index=False)
     # Save the stacked out-of-fold predictions
-    df_stacked_out_of_fold_preds[model_name] = np.concatenate(out_of_fold_preds_list)
-    df_stacked_out_of_fold_preds\
-    .to_csv(stacked_preds_csv, index=False)
+    df_stacked_out_of_fold_preds[model_name] = out_of_fold_preds
+    df_stacked_out_of_fold_preds.to_csv(result_directory + 'stacked_preds.csv', index=False)
     # Save the temporal predictions
-    df_preds_tmp.to_csv(temporal_preds_csv)
+    df_preds_tmp.to_csv(result_directory + 'temporal_preds.csv', index=False)
